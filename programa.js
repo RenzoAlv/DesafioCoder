93647b9ea5b05835c4c5bec0cdde1a1fc8b5fd1f
RenzoAlvarado <renzoalv1@hotmail.com>
2021-11-07 16:38:00 -0300

    Desafio 1 y 2

---
+++ programa.js
@@ -0,0 +1,10 @@
+
+let edad= prompt("Ingresa tu edad");
+
+if(edad>=18){
+    alert ("Usted puede ingresar");
+}
+else if(edad>=16){
+    alert("Puede ingresar con un mayor");
+}
+else{alert ("usted no puede ingresar");}
